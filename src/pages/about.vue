988febb4f220e525102e5e08eddf967de563412a
Diego Cano <diego.cano@uabc.edu.mx>
2026-03-09 19:52:36 -0700

    Componente LoadingSpinner implementado

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -2,10 +2,10 @@
   <v-container class="text-white text-center pa-10">
     <v-card class="bg-grey-darken-3 pa-6 mx-auto" max-width="500">
       <v-icon size="80" color="blue">mdi-movie-open</v-icon>
-      <h1 class="text-h4 my-4">Movie Search App</h1>
-      <p class="text-body-1">Versión 1.0.0</p>
+      <h1 class="text-h4 my-4">Buscador de peliculas</h1>
+        <p>Proyecto desarrollado por Diego Cano para la materia de Aplicaciones Web.</p>
       <v-divider class="my-6"></v-divider>
-      <p>Práctica de Laboratorio: Comunicación entre componentes y API de OMDb.</p>
+      <p>Comunicación entre componentes y API de OMDb.</p>
       <v-btn to="/" color="blue" class="mt-6">Volver al Buscador</v-btn>
     </v-card>
   </v-container>
